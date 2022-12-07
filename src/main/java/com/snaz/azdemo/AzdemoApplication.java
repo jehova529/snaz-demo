9d8461e601a30e1824c635629c6a0661a690beb8
jehova529 <55429509+jehova529@users.noreply.github.com>
2022-12-07 16:40:40 +0530

    Add files via upload

---
+++ src/main/java/com/snaz/azdemo/AzdemoApplication.java
@@ -0,0 +1,13 @@
+package com.snaz.azdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzdemoApplication.class, args);
+	}
+
+}
